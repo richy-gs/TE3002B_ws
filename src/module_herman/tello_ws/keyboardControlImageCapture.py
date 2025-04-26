7dc4ff4d36a1b785fd678db92749c59111fc0246
richy-gs <jesusrg2405@gmail.com>
2025-04-25 23:59:51 -0600

    Start new project

---
+++ src/module_herman/tello_ws/keyboardControlImageCapture.py
@@ -0,0 +1,52 @@
+import cv2
+import keyPressModule as kp
+from djitellopy import Tello
+
+kp.init()
+me = Tello()
+me.connect()
+print(me.get_battery())
+
+me.streamon()
+
+
+def getKeyboardInput():
+    lr, fb, ud, yv = 0, 0, 0, 0
+    speed = 50
+
+    if kp.getKey("LEFT"):
+        lr = -speed
+    elif kp.getKey("RIGHT"):
+        lr = speed
+
+    if kp.getKey("UP"):
+        fb = speed
+    elif kp.getKey("DOWN"):
+        fb = -speed
+
+    if kp.getKey("w"):
+        ud = speed
+    elif kp.getKey("s"):
+        ud = -speed
+
+    if kp.getKey("a"):
+        yv = speed
+    elif kp.getKey("d"):
+        yv = -speed
+
+    if kp.getKey("q"):
+        me.land()
+    if kp.getKey("e"):
+        me.takeoff()
+
+    return [lr, fb, ud, yv]
+
+
+while True:
+    vals = getKeyboardInput()
+    me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+
+    img = me.get_frame_read().frame
+    img = cv2.resize(img, (360, 240))
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
