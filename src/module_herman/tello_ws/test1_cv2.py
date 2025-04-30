6ff67b53da8f801c151a1f332420e689b69095c3
richy-gs <jesusrg2405@gmail.com>
2025-04-29 19:11:08 -0600

    Modified position of circle

--- src/module_herman/tello_ws/test1_cv2.py
+++ src/module_herman/tello_ws/test1_cv2.py
@@ -42,19 +42,23 @@
 # sys.exit()
 
 
-import numpy as np
 import cv2
+import numpy as np
+
 
 def drawPoints(img):
-    cv2.circle(img, (50, 50), 5, (0, 0, 255), cv2.FILLED)  # Coordenadas adaptadas a imagen de 100x100
+    cv2.circle(
+        img, (50, 50), 5, (0, 0, 255), cv2.FILLED
+    )  # Coordenadas adaptadas a imagen de 100x100
+
 
 while True:
-    img = np.zeros((100, 100, 3), np.uint8)
+    img = np.zeros((1000, 1000, 3), np.uint8)
     drawPoints(img)
     cv2.imshow("Output", img)
-    
+
     # Presiona 'q' para salir del bucle
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cv2.destroyAllWindows()
